aae5bc010c15dc7b573bcff566a65ecd7a1dce84
italomoralesf <i@italomoralesf.com>
2017-07-14 07:32:51 -0500

    controllers, views, router

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,22 +9,9 @@
     </head>
     <body>
         <div id="main" class="container">
-            <div class="row">
-                <div class="col-sm-4">
-                    <h1>VUEjs - AJAX axios</h1>
-                    <ul class="list-group">
-                        <li v-for="item in lists" class="list-group-item">                      
-                            @{{ item.name }}
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-sm-8">
-                    <h1>JSON</h1>
-                    <pre>
-                        @{{ $data }}
-                    </pre>
-                </div>
-            </div>
+            
+            @yield('content')
+
         </div>
         
         <script src="{{ asset('js/app.js') }}"></script>
