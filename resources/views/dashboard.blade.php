ed17cb2e6e3054afdfa76ae36a2c1b66195e44bf
italomoralesf <i@italomoralesf.com>
2017-08-24 22:40:12 -0500

    initial create laravel & vue

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,9 @@
 		<h1 class="page-header">CRUD Laravel y VUEjs</h1>
 	</div>
 	<div class="col-sm-7">
-		<a href="#" class="btn btn-primary pull-right">Nueva tarea</a>
+		<a href="#" class="btn btn-primary pull-right" data-toggle="modal" data-target="#create">
+		Nueva tarea
+		</a>
 		<table class="table table-hover table-striped">
 			<thead>
 				<tr>
@@ -31,6 +33,7 @@
 				</tr>
 			</tbody>
 		</table>
+		@include('create')
 	</div>
 	<div class="col-sm-5">
 		<pre>
